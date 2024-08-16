8d6aee1499e2a0e9daa314e7ac29821f7e61a64e
PabloJY <2959114680406@ingenieria.usac.edu.gt>
2024-08-15 23:28:56 -0600

    Primera estructura de IDE

---
+++ generalScript.js
@@ -0,0 +1,17 @@
+function mostrarConsola(evt, consoleName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(consoleName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+function mensajeAlert(mensaje){
+    window.alert(mensaje)
+}
